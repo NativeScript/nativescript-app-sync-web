c110ea31b036502097a595ccfd0eb181d2d15fb3
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-08-01 16:52:42 +0200

    The homepage shows an error after a few seconds #2 (this is me, commenting a few things.. because I'm a bit at a loss)

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,17 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { persistState } from 'redux-devtools';
+// import { persistState } from 'redux-devtools';
 import rootReducer from '../reducers';
 import createHelpers from './createHelpers';
-import createLogger from './logger';
-import DevTools from '../components/DevTools';
+// import createLogger from './logger';
+// import DevTools from '../components/DevTools';
 
 export default function configureStore(initialState, helpersConfig) {
-  const helpers = createHelpers(helpersConfig);
-  const middleware = [thunk.withExtraArgument(helpers)];
+  // const helpers = createHelpers(helpersConfig);
+  // const middleware = [thunk.withExtraArgument(helpers)];
 
   let enhancer;
 
+  /*
   if (__DEV__) {
     middleware.push(createLogger());
 
@@ -31,17 +32,18 @@ export default function configureStore(initialState, helpersConfig) {
       devToolsExtension,
     );
   }
+  */
 
   // See https://github.com/rackt/redux/releases/tag/v3.1.0
   const store = createStore(rootReducer, initialState, enhancer);
 
   // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
-  if (__DEV__ && module.hot) {
-    module.hot.accept('../reducers', () =>
-      // eslint-disable-next-line global-require
-      store.replaceReducer(require('../reducers').default),
-    );
-  }
+  // if (__DEV__ && module.hot) {
+  //   module.hot.accept('../reducers', () =>
+  //     eslint-disable-next-line global-require
+      // store.replaceReducer(require('../reducers').default),
+    // );
+  // }
 
   return store;
 }
