aa537462a167740de6d8111abf432f3575876742
Igor Randjelovic <rigor789@gmail.com>
2022-02-15 13:20:47 +0100

    fix: read REACT_APP_API_URL environment variable

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import Axios from 'axios'
 import { getToken } from './auth';
 
 const axiosInstance = Axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API_URL || process.env.API_URL || 'http://localhost:5000',
   timeout: 1000
 });
 
